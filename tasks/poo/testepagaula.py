68a2677d7d9170b5125c5d5d8996f8c31ed45577
Esdras Dos Anjos <esdrasanjosdesigner@gmail.com>
2025-12-14 23:14:52 -0300

    zoo

---
+++ tasks/poo/testepagaula.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+class MetodoPag(ABC):
+    @abstractmethod
+    def processar_pag(self, valor: float, desc: str):
+        pass
+
+class Pagamento:
+    def __init__(self, valor: float, desc: str, metodo_pag: MetodoPag):
+        self.valor: float = valor
+        self.desc: str = desc
+        self.metodo: MetodoPag = metodo_pag
+
+    def resumo(self):
+        print(f"Pagamento de R$ {self.valor}: {self.desc}")
+    def validar_valor(self):
+        if self.valor <= 0:
+            raise ValueError("Valor invalido")
+    
+    def processar(self):
+        self.validar_valor()
+        self.resumo()
+        self.metodo.processar_pag(self.valor, self.desc)
+
+class MetodoPix(MetodoPag):
+    def __init__(self, chave: str):
+        self.chave: str = chave
+
+    def get_chave(self):
+        return self.chave
+
+    def processar_pag(self, valor: float, desc: str):
+        print(f"Pagando com pix chave {self.chave}, valor {valor}, produto {desc}")
+
+def processar_pagamento(pag: Pagamento):
+    pag.validar_valor()
+    pag.resumo()
+    pag.processar()
+
+metodoPix = MetodoPix("pagar@picpay.com")
+metodoCartao = MetodoCartao("123123123", 500)
+pagamento = Pagamento(desc="salgado", valor=5.90, metodo_pag=metodoPix)
+pagamento.processar()
+
+# ACOPLAMENTO
+# dependencia baixa
+# dependencia alta
